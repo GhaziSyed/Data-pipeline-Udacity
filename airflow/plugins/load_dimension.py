46550bdd0af6ca46363c448e2aa6df2386e30449
GhaziSyed <115798228+GhaziSyed@users.noreply.github.com>
2023-06-23 20:46:25 +0200

    Update load_dimension.py

--- airflow/plugins/load_dimension.py
+++ airflow/plugins/load_dimension.py
@@ -1,137 +1,45 @@
-from datetime import datetime, timedelta
-import pendulum
-import os
-from airflow.decorators import dag
-from airflow.operators.dummy_operator import DummyOperator
-from final_project_operators.stage_redshift import StageToRedshiftOperator
-from final_project_operators.load_fact import LoadFactOperator
-from final_project_operators.load_dimension import LoadDimensionOperator
-from final_project_operators.data_quality import DataQualityOperator
-from udacity.common import final_project_sql_statements
+from airflow.hooks.postgres_hook import PostgresHook
+from airflow.models import BaseOperator
+from airflow.utils.decorators import apply_defaults
 
-# Properties of DAG object
-default_args = {
-    'owner': 'udacity',
-    'start_date': pendulum.now()
-    'depends_on_past': False,
-    'retries': 3,
-    'retry_delay': timedelta(minutes=5),
-    'catchup': False,
-    'email_on_retry': False
-}
+class LoadDimensionOperator(BaseOperator):
+    ui_color = '#80BD9E'
 
-# Instantiate a DAG object using Airflow 1 code style
-@dag(
-    default_args=default_args,
-    description='Load and transform data in Redshift with Airflow',
-    schedule_interval='0 * * * *'
-)
-# Instantiate the pipline function
-def final_project():
+    # SQL query for f-string execution
+    insert_sql = """
+        INSERT INTO {}
+        {};
+    """
+    truncate_sql = """
+        TRUNCATE TABLE {};
+    """
+    # Provide default values for the method
+    @apply_defaults
+    def __init__(self,
+                 table="",
+                 redshift_conn_id="",
+                 load_sql_stmt="",
+                 truncate_table=False,
+                 *args, **kwargs):
+    # Instantiate with user defined values
+        super(LoadDimensionOperator, self).__init__(*args, **kwargs)
+        self.table = table
+        self.redshift_conn_id = redshift_conn_id
+        self.load_sql_stmt = load_sql_stmt
+        self.truncate_table = truncate_table
+    # Execute the operator's functionality      
+    def execute(self, context):
+        redshift = PostgresHook(postgres_conn_id=self.redshift_conn_id)
+        
+        if self.truncate_table:
+            self.log.info(f"Truncating dimension table: {self.table}")
+            redshift.run(LoadDimensionOperator.truncate_sql.format(self.table))
 
-    '''Create the Airflow tasks for automated ETL of Sparkify data'''
-
-    start = DummyOperator(task_id='Begin_Pipeline')
-
-    stage_events_to_redshift = StageToRedshiftOperator(
-        task_id='Stage_events',
-        dag=dag,
-        table="staging_events",
-        redshift_conn_id="redshift",
-        aws_credentials_id="aws_credentials",
-        s3_bucket="udacity-dend",
-        s3_key="log_data",
-        region="us-east-1",
-        extra_params="FORMAT AS JSON 's3://udacity-dend/log_json_path.json'"
-    )
-
-    stage_songs_to_redshift = StageToRedshiftOperator(
-        task_id='Stage_songs',
-        dag=dag,
-        table="staging_songs",
-        redshift_conn_id="redshift",
-        aws_credentials_id="aws_credentials",
-        s3_bucket="udacity-dend",
-        s3_key="song_data/A/A/A",
-        region="us-east-1",
-        extra_params="JSON 'auto' COMPUPDATE OFF"
-    )
-
-    load_songplays_table = LoadFactOperator(
-        task_id='Load_songplays_fact_table',
-        dag=dag,
-        table='songplays',
-        redshift_conn_id="redshift",
-        load_sql_stmt=SqlQueries.songplay_table_insert
-    )
-
-    load_user_dimension_table = LoadDimensionOperator(
-        task_id='Load_user_dim_table',
-        dag=dag,
-        table='users',
-        redshift_conn_id="redshift",
-        truncate_table=True,
-        load_sql_stmt=SqlQueries.user_table_insert
-    )
-
-    load_song_dimension_table = LoadDimensionOperator(
-        task_id='Load_song_dim_table',
-        dag=dag,
-        table='songs',
-        redshift_conn_id="redshift",
-        truncate_table=True,
-        load_sql_stmt=SqlQueries.song_table_insert
-    )
-
-    load_artist_dimension_table = LoadDimensionOperator(
-        task_id='Load_artist_dim_table',
-        dag=dag,
-        table='artists',
-        redshift_conn_id="redshift",
-        truncate_table=True,
-        load_sql_stmt=SqlQueries.artist_table_insert
-    )
-
-    load_time_dimension_table = LoadDimensionOperator(
-        task_id='Load_time_dim_table',
-        dag=dag,
-        table='time',
-        redshift_conn_id="redshift",
-        truncate_table=True,
-        load_sql_stmt=SqlQueries.time_table_insert
-    )
-
-    quality_checks = DataQualityOperator(
-    task_id='data_quality_checks',
-    dag=dag,
-    dq_checks=[
-        { 'check_sql': 'SELECT COUNT(*) FROM public.songplays WHERE userid IS NULL', 'expected_result': 0 },
-        { 'check_sql': 'SELECT COUNT(DISTINCT "level") FROM public.songplays', 'expected_result': 2 }
-    ],
-    redshift_conn_id="redshift"
-    )
-
-    end = DummyOperator(task_id='End_Pipeline',  dag=dag)
-
-    '''Order the execution of the tasks'''
-
-    # Start Airflow Pipeline 
-    start >> stage_events_to_redshift
-    start >> stage_songs_to_redshift
-    # Create Staging Tables
-    stage_events_to_redshift >> load_songplays_table
-    stage_songs_to_redshift >> load_songplays_table
-    # Create Destination Tables
-    load_songplays_table >> load_user_dimension_table
-    load_songplays_table >> load_song_dimension_table
-    load_songplays_table >> load_artist_dimension_table
-    load_songplays_table >> load_time_dimension_table
-    # Perform Data Quality Check
-    load_user_dimension_table >> run_quality_checks
-    load_song_dimension_table >> run_quality_checks
-    load_artist_dimension_table >> run_quality_checks
-    load_time_dimension_table >> run_quality_checks
-    # End Airflow Pipeline 
-    quality_checks >> end
-
-final_project_dag = final_project()
+        self.log.info(f"Loading dimension table {self.table}")
+        formatted_sql = LoadDimensionOperator.insert_sql.format(
+            self.table,
+            self.load_sql_stmt
+        )
+        
+        self.log.info(f"Executing Query: {formatted_sql}")
+        redshift.run(formatted_sql)
